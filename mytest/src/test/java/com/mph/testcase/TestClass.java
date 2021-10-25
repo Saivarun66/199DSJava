1e2ba9eab47059889366e9625ff8e36b1f0255ee
Saivarun <saivarun66@gmail.com>
2021-10-25 12:05:20 +0530

    hello

---
+++ mytest/src/test/java/com/mph/testcase/TestClass.java
@@ -0,0 +1,61 @@
+package com.mph.testcase;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestClass {
+
+	@Test
+	public void testAllAssertMethods() {
+		String str1 = new String("Rahul");
+		String str2 = new String("Rahul");
+		String str3 = null;
+		String str4 = "deeps";
+		String str5 = "deepss";
+
+		int val1 = 5;
+		int val2 = 6;
+
+		 assertEquals(str4, str5);
+		 //assertSame(str4, str5);
+		// assertNotSame(str1, str2);
+		// assertTrue(val1<val2);
+		// assertFalse(val1<val2);
+		// assertNotNull(str3);
+
+	}
+
+	@Test
+	public void testAllAraayMethods() {
+
+		String[] arr1 = { "one", "two", "three" };
+		String[] arr2 = { "one", "two", "three" };
+
+		assertArrayEquals(arr1, arr2);
+
+	}
+	
+	@Test
+	public void testAllNumberMethods() {
+		
+		int val1 = 5;
+		int val2 = 6;
+
+		
+		 assertTrue(val1<val2);
+		 
+		 //assertFalse(val1<val2);
+		
+
+	}
+
+
+}
